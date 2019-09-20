f2cbeb2b1ee1f3696b7b031d783c757d0838a83d
Nikhil Vijayan <nkhilv@gmail.com>
2019-09-20 13:40:39 +0530

    :recycle: delete comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-// export const Button = ({ text, onClick, type, disabled }) => (
-//   <button
-//     type={type}
-//     disabled={disabled}
-//     onClick={onClick}
-//     className={styles.button}
-//   >
-//     {text}
-//   </button>
-// );
-
 export default function Button({ text, path, className }) {
   return (
     <button className={className} type="button">
