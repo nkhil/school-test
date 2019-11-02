96a23ffdd25752243063019b71a7f490a393e5e6
Nikhil Vijayan <nkhilv@gmail.com>
2019-11-02 22:47:45 +0000

    :lipstick: remove border radius from button

--- src/components/styledComponents/Button.js
+++ src/components/styledComponents/Button.js
@@ -5,10 +5,9 @@ const StyledButton = styled(Button)`
   margin: 20px 0;
 
   a {
-    background-color: #da4a21;
+    background-color: #000;
     color: #fff;
     padding: 10px 20px;
-    border-radius: 10px;
     text-decoration: none;
     text-transform: uppercase;
     font-size: 1.5rem;
